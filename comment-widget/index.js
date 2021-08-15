1710bf9638d546ce19e7a14c23d14bd8d51f751b
abhisheksarmah <abhisheksarmah660@gmail.com>
2021-08-15 20:52:29 +0530

    code refactor comment widget

--- comment-widget/index.js
+++ comment-widget/index.js
@@ -1,142 +1,136 @@
+const comments = [
+    {
+        "id": 1,
+        "comment": "I am alive",
+        "author": "Abhishek",
+        "childrens": [
+            {
+                "id": 2,
+                "comment": "thanks for comment",
+                "author": "Abhilekh",
+                "childrens": [
+                    {
+                        "id": 3,
+                        "comment": "I am alive",
+                        "author": "Abhishek",
+                        "childrens": [
+                            {
+                                "id": 4,
+                                "comment": "thanks for comment",
+                                "author": "Abhilekh",
+                                "childrens": []
+                            },
+                            {
+                                "id": 5,
+                                "comment": "very good day",
+                                "author": "Susmita",
+                                "childrens": []
+                            }
+                        ]
+                    },
+                    {
+                        "id": 6,
+                        "comment": "I am alive",
+                        "author": "Abhishek",
+                        "childrens": [
+                            {
+                                "id": 6,
+                                "comment": "thanks for comment",
+                                "author": "Abhilekh",
+                                "childrens": []
+                            },
+                            {
+                                "id": 7,
+                                "comment": "very good day",
+                                "author": "Susmita",
+                                "childrens": []
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "id": 8,
+                "comment": "very good day",
+                "author": "Susmita",
+                "childrens": []
+            }
+        ]
+    },
+    {
+        "id": 9,
+        "comment": "I am not alive",
+        "author": "Abhishek",
+        "childrens": [
+        ]
+    },
+    {
+        "id": 10,
+        "comment": "I am going to be alive",
+        "author": "Abhishek",
+        "childrens": [
+            {
+                "id": 11,
+                "comment": "thanks for comment",
+                "author": "Abhilekh",
+                "childrens": []
+            },
+            {
+                "id": 12,
+                "comment": "very good day",
+                "author": "Susmita",
+                "childrens": []
+            }
+        ]
+    }
+];
 let lastCommentId = 12;
-const commentBox = document.getElementById('comment-box');
+const commentFormWrapper = document.getElementById('comment-form-wrapper');
+const commentArea = document.getElementById('comment-area');
+
 function getCommentElement(commentId) {
     return document.getElementById(`comment${commentId}`);
 }
 
-const getComments = function() {
-    const data = [
-        {
-            "id": 1,
-            "comment": "I am alive",
-            "author": "Abhishek",
-            "childrens": [
-                {
-                    "id": 2,
-                    "comment": "thanks for comment",
-                    "author": "Abhilekh",
-                    "childrens": [
-                        {
-                            "id": 3,
-                            "comment": "I am alive",
-                            "author": "Abhishek",
-                            "childrens": [
-                                {
-                                    "id": 4,
-                                    "comment": "thanks for comment",
-                                    "author": "Abhilekh",
-                                    "childrens": []
-                                },
-                                {
-                                    "id": 5,
-                                    "comment": "very good day",
-                                    "author": "Susmita",
-                                    "childrens": []
-                                }
-                            ]
-                        },
-                        {
-                            "id": 6,
-                            "comment": "I am alive",
-                            "author": "Abhishek",
-                            "childrens": [
-                                {
-                                    "id": 6,
-                                    "comment": "thanks for comment",
-                                    "author": "Abhilekh",
-                                    "childrens": []
-                                },
-                                {
-                                    "id": 7,
-                                    "comment": "very good day",
-                                    "author": "Susmita",
-                                    "childrens": []
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    "id": 8,
-                    "comment": "very good day",
-                    "author": "Susmita",
-                    "childrens": []
-                }
-            ]
-        },
-        {
-            "id": 9,
-            "comment": "I am not alive",
-            "author": "Abhishek",
-            "childrens": [
-            ]
-        },
-        {
-            "id": 10,
-            "comment": "I am going to be alive",
-            "author": "Abhishek",
-            "childrens": [
-                {
-                    "id": 11,
-                    "comment": "thanks for comment",
-                    "author": "Abhilekh",
-                    "childrens": []
-                },
-                {
-                    "id": 12,
-                    "comment": "very good day",
-                    "author": "Susmita",
-                    "childrens": []
-                }
-            ]
-        }
-    ];
-    return new Promise((resolve) => {
-        resolve(data);
-    })
-}
-
-getComments().then((response) => {
-    const commentArea = document.querySelector('#comment-area');
-    mapComments(response, commentArea);
-})
-
-function printCommentDetails(comment) {
-    return `<div id="comment${comment.id}" class="reply-section">
-        <div style="display: flex; align-items: center;">
-            <p class="comment-author">Author: ${comment.author} </p>
-            <a href="#">Edit</a>
+function getCommentCard(comment) {
+    return `<div id="comment${comment.id}" class="media">
+        <div class="media-object"/>&#x1F60E;</div>
+        <div class="media-body">
+            <h3 class="media-heading">${comment.author}</h3>
+            <p>${comment.comment} </p>
+            <div class="media-footer">
+                <a href="#">Edit</a>
+                <a href="#" onclick="clickReplyButton(${comment.id})">Reply</a>
+            </div>
         </div>
-        <p>${comment.comment}</p>
-        <a href="#" onclick="clickReply(${comment.id})">Reply</a>
-    </div>`;
+    </div>`;    
 }
 
-const mapComments = function(comments, element) {
+const generateCommentListView = function(comments, element) {
     comments.forEach(comment => {
 
         const li = document.createElement('li');
-        li.innerHTML = printCommentDetails(comment);
+        li.innerHTML = getCommentCard(comment);
         element.appendChild(li);
 
-        if (comment.childrens.length > 0) {
+        if (comment.childrens && comment.childrens.length > 0) {
             const ul = document.createElement('ul');
             li.appendChild(ul);
-            mapComments(comment.childrens, ul);
+            generateCommentListView(comment.childrens, ul);
         }
     })
 }
 
-const clickReply = (commentId) => {
+const clickReplyButton = (commentId) => {
     const comment = getCommentElement(commentId);
     let formElement = document.getElementById(`form${commentId}`);
     if(!formElement) {
         formElement = document.createElement('form');
-        formElement.setAttribute('onsubmit', `replyOnComment(this, ${commentId})`);
+        formElement.setAttribute('onsubmit', `submitReply(this, ${commentId})`);
         formElement.setAttribute('id', `form${commentId}`); 
-        formElement.innerHTML = commentBox.innerHTML;
-
-        comment.appendChild(formElement)
+        formElement.setAttribute('class', 'form-wrapper'); 
+        formElement.innerHTML = commentFormWrapper.innerHTML;
+        comment.getElementsByClassName('media-body')[0].appendChild(formElement)
     } else {
         if (formElement.style.display === 'none') {
             formElement.style.display = 'block';
@@ -146,26 +140,26 @@ const clickReply = (commentId) => {
     }  
 }
 
-const replyOnComment = (formElement, commentId) => {
+const submitReply = (formElement, commentId) => {
     const repliedComment = formElement.elements[0].value;
     lastCommentId = lastCommentId + 1;
     const comment = getCommentElement(commentId);
+    const commentDetails = [{
+        id: lastCommentId,
+        comment: repliedComment,
+        author: 'Abhishek'
+    }]
 
     let ul = comment.nextElementSibling;
     if (!ul) {
         ul = document.createElement('ul');
-        comment.appendChild(ul);
+        comment.insertAdjacentElement('afterend', ul);
     }
 
-    const li = document.createElement('li');
+    generateCommentListView(commentDetails, ul)
 
-    const commentDetails = {
-        id: lastCommentId,
-        comment: repliedComment,
-        author: 'Abhishek'
-    }
-    li.innerHTML = printCommentDetails(commentDetails);
-    ul.appendChild(li);
-
+    formElement.elements[0].value = '';
     formElement.style.display = 'none';
 }
+
+generateCommentListView(comments, commentArea);
